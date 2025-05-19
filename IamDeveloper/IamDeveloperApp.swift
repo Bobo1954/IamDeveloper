908640db8b4191d1018a4fd9742602cf1c604556
Louis Kudelka <aelnk1954@gmail.com>
2025-05-19 14:49:24 -0400

    Initial Commit

---
+++ IamDeveloper/IamDeveloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  IamDeveloperApp.swift
+//  IamDeveloper
+//
+//  Created by Mbam3 on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct IamDeveloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
