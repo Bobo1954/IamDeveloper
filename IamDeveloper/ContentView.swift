6bdb8bfe6538ccaa60e339a75a8974851f46e25a
Louis Kudelka <aelnk1954@gmail.com>
2025-05-26 15:35:57 -0400

    animation of text and image

--- IamDeveloper/ContentView.swift
+++ IamDeveloper/ContentView.swift
@@ -12,32 +12,41 @@ struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var imageNumber = 0
+    @State private var messNumber = 0
     
     var body: some View {
         VStack {
             
-            Spacer()
-            
+            //Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .frame(height: 100)
+                .minimumScaleFactor(0.5)
+                .animation(.easeInOut(duration: 0.15), value: message)
+                
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 40))
                 .shadow(radius: 40)
                 .padding(20)
-                
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .animation(.default, value: imageNumber)
             
             Spacer()
             
             Button("Show Message") {
                 
-                let strAwesome = "You Are Awesome!"
-                let strGreate = "You Are Greate!"                
+                let messArry = ["You Are Awesome!", "You Are Greate!", "Fabulous? That's You!", "You are braver than you believe, stronger than you seem, and smarter than you think.","Believe you can and you're halfway there.","The future belongs to those who believe in the beauty of their dreams.", "Your time is limited, don't waste it living someone else's life."]
                 
-                message = (message == strAwesome ? strGreate : strAwesome)
+                if messNumber == messArry.count{
+                    messNumber = 0
+                }
+                message = messArry[messNumber]
+                messNumber += 1
+                
                 if imageNumber == 8 {
                     imageNumber = 0
                 }
